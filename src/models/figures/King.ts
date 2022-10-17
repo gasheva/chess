2dd270c524ff99a6756fc828940ef698a516b671
Gasheva Tanya <gasheva_t@cobalt42.ru>
2022-10-17 10:49:38 +0500

    Add king moving rules

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -14,6 +14,7 @@ export class King extends Figure {
 
     canMove(target: Cell): boolean {
         if (!super.canMove(target)) return false;
-        return true;
+        if (Math.abs(target.x - this.cell.x) <= 1 && Math.abs(target.y - this.cell.y) <= 1) return true;
+        return false;
     }
 }
