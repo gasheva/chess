0d8d55b03c6517b3cabb5883c0c947779a4078bc
Gasheva Tanya <gasheva_t@cobalt42.ru>
2022-10-17 08:58:29 +0500

    Add models

---
+++ src/models/Cell.ts
@@ -0,0 +1,25 @@
+import {Colors} from './Colors';
+import {Figure} from './figures/Figure';
+import {Board} from './Board';
+
+export class Cell {
+    readonly x: number;
+    readonly y: number;
+    readonly color: Colors;
+    figure: Figure | null;
+    // cycle dependency - Cell know about Board and Board know about Cell
+    board: Board;
+    available: boolean; // can move
+    id: number; // for react keys
+
+    constructor(board: Board, x: number, y: number, color: Colors, figure: Figure | null) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.figure = figure;
+        this.board = board;
+        this.available = false;
+        this.id = Math.random()
+    }
+
+}
