82664f03f1d183583ca930411afcd89454f04aec
Gasheva Tanya <gasheva_t@cobalt42.ru>
2022-10-17 09:22:15 +0500

    Add Board and Cell components. Add Figure model

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Board} from './models/Board';
+import BoardComponent from './components/BoardComponent';
 
 const App = () => {
+    const [board, setBoard] = useState(new Board());
+
+    useEffect(() => {
+        restart();
+    }, []);
+
+    function restart() {
+        const newBoard = new Board();
+        newBoard.initCells();
+        setBoard(newBoard);
+    }
+
     return (
         <div className="app">
+            <BoardComponent board={board} setBoard={setBoard}/>
         </div>
     );
 };
